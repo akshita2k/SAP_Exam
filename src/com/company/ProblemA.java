c7adc49b693b60f3e1aa8d04ee2ef280d4baf3e3
akshita2k <akshita2k@gmail.com>
2021-06-11 23:41:26 +0530

    Prints the distinct element from an array

--- src/com/company/ProblemA.java
+++ src/com/company/ProblemA.java
@@ -2,30 +2,37 @@ package com.company;
 import java.util.Scanner;
 
 public class ProblemA {
-    //public static int diff(int n, int arr[]) {}
+    static int diff(int n, int arr[]) {
+        Scanner sc = new Scanner (System.in);
+        int res =0;
+        for (int i=0; i<n; i++) {
+            int j;
+            for ( j=0; j<n; j++) {
+                if (i!=j && arr[i]==arr[j]) {
+
+                    break;
+                }
+            }
+            if (j==n) {
+                res = arr[i];
+            }
+        }
+        return res;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        int []a = new int [t];
         for (int z=0; z<t; z++){
             int n = sc.nextInt();
             int [] arr = new int [n];
             for (int i=0; i<n; i++) {
                 arr[i] = sc.nextInt();
             }
-            int res =0;
-            for (int i=0; i<n; i++) {
-                int j;
-                for ( j=0; j<n; j++) {
-                    if (i!=j && arr[i]==arr[j]) {
-
-                        break;
-                    }
-                }
-                if (j==n) {
-                    res = arr[i];
-                }
-            }
-            System.out.println(res);
+            a[z] = diff(n,arr);
+        }
+        for (int i=0; i<t; i++) {
+            System.out.println(a[i]);
         }
     }
 }
